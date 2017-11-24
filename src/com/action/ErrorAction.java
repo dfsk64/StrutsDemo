c9b16143787f408492c8e72514dca83e20443cff
wangfeng <dfsk64@outlook.com>
2017-11-24 14:54:20 +0800

    error page

---
+++ src/com/action/ErrorAction.java
@@ -0,0 +1,21 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ErrorAction extends ActionSupport {
+	private String errMsg;
+	
+	@Override
+	public String execute() throws Exception {
+		this.errMsg = "This is my custom error page";
+		return SUCCESS;
+	}
+	
+	public String getErrMsg() {
+		return this.errMsg;
+	}
+	
+	public void setErrMsg(String errMsg) {
+		this.errMsg = errMsg;
+	}
+}
