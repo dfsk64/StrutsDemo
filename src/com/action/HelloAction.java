4765c193a1e2b166df78e69212b97337e2b0c21f
wangfeng <dfsk64@outlook.com>
2017-11-30 16:45:22 +0800

    hibernate

--- src/com/action/HelloAction.java
+++ src/com/action/HelloAction.java
@@ -1,5 +1,11 @@
 package com.action;
 
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloAction extends ActionSupport {
@@ -7,6 +13,24 @@ public class HelloAction extends ActionSupport {
 
 	@Override
 	public String execute() throws Exception {
+		Configuration cfg = new Configuration().configure();
+
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		Session session = null;
+		session = factory.openSession();
+		session.beginTransaction();
+
+		com.bean.User user = new com.bean.User();
+		user.setUsername("hibernate");
+		user.setPassword("123456");
+		user.setCreateTime(new Date());
+		user.setExpireTime(new Date());
+
+		session.save(user);
+		session.getTransaction().commit();
+		session.close();
+
 		if ("SECRET".equals(message)) {
 			return SUCCESS;
 		}
